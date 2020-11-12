4a8d2cb851808f841eef00f5f9ee5ff8f830c06b
nguyentan2212 <nguyentan2212@gmail.com>
2020-11-12 14:22:33 +0700

    update project to net5.0

--- PhoneStoreWeb.Data/Models/Order.cs
+++ PhoneStoreWeb.Data/Models/Order.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Runtime.Serialization;
-using Twilio.Types;
 
 namespace PhoneStoreWeb.Data.Models
 {
