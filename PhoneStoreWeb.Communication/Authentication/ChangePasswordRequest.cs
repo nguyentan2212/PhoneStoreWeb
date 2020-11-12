54e1fd35eb2da67bf36ce6b4962f4c8734d2cebe
nguyentan2212 <57379741+nguyentan2212@users.noreply.github.com>
2020-11-12 22:29:49 +0700

    Create change password request model

---
+++ PhoneStoreWeb.Communication/Authentication/ChangePasswordRequest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneStoreWeb.Communication.Authentication
+{
+    public class ChangePasswordRequest
+    {
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string NewPassword { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("New Password", ErrorMessage = "New password and confirm password are not matched")]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        public Guid UserId { get; set; }
+    }
+}
