54ecf69e50a104b9a4a660ff22aab0959c26b182
nguyentan2212 <57379741+nguyentan2212@users.noreply.github.com>
2020-11-12 22:30:15 +0700

    Create service base

---
+++ PhoneStoreWeb.API/Services/Base/IServiceBase.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.Collections.Generic;
+
+namespace PhoneStoreWeb.API.Services.Base
+{
+    public interface IServiceBase
+    {
+        public IEnumerable<string> GetErrors(ModelStateDictionary modelState);
+        public IEnumerable<string> GetErrors(IdentityResult identityResult);
+    }
+}
---
+++ PhoneStoreWeb.API/Services/Base/ServiceBase.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PhoneStoreWeb.API.Services.Base
+{
+    public abstract class ServiceBase : IServiceBase
+    {       
+        public IEnumerable<string> GetErrors(ModelStateDictionary modelState)
+        {
+            IEnumerable<string> errors = modelState.Values.SelectMany(v => v.Errors).Select(x => x.ErrorMessage);
+            return errors;
+        }
+
+        public IEnumerable<string> GetErrors(IdentityResult identityResult)
+        {
+            IEnumerable<string> errors = identityResult.Errors.Select(x => x.Description);
+            return errors;
+        }
+    }
+}
