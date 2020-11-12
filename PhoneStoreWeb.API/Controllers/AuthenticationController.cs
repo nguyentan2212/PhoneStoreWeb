a9944c7f8f9af75330438e85e5b427f5a7568211
nguyentan2212 <57379741+nguyentan2212@users.noreply.github.com>
2020-11-12 22:35:09 +0700

    Add Register, ChangePassword and SetRole action

--- PhoneStoreWeb.API/Controllers/AuthenticationController.cs
+++ PhoneStoreWeb.API/Controllers/AuthenticationController.cs
@@ -1,48 +1,139 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using PhoneStoreWeb.Communication;
 using PhoneStoreWeb.Communication.Authentication;
-using PhoneStoreWeb.Communication.Message;
+using PhoneStoreWeb.Communication.ResponseResult;
 using PhoneStoreWeb.Data.Models;
 using PhoneStoreWeb.Services.Authentication;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace PhoneStoreWeb.API.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/[controller]")]
     public class AuthenticationController: ControllerBase
     {
         private readonly IAuthService authService;
         private readonly UserManager<AppUser> userManager;
         private readonly RoleManager<AppRole> roleManager;
         private readonly SignInManager<AppUser> signInManager;
+        private readonly IMapper mapper;
 
-        public AuthenticationController(IAuthService authService, SignInManager<AppUser> signInManager, RoleManager<AppRole> roleManager)
+        public AuthenticationController(IAuthService authService, SignInManager<AppUser> signInManager, RoleManager<AppRole> roleManager, IMapper mapper)
         {
             this.authService = authService;
             this.roleManager = roleManager;
             this.signInManager = signInManager;
-            this.userManager = this.signInManager.UserManager;
+            userManager = this.signInManager.UserManager;
+            this.mapper = mapper;
         }
 
-        public async Task<ActionResult<AuthResponse>> Login([FromBody] LoginRequest request)
+        [HttpGet("login")]
+        public async Task<ActionResult<ResponseResult<AuthResponse>>> Login([FromBody] LoginRequest request)
         {
+            ResponseResult<AuthResponse> response = new ResponseResult<AuthResponse>();
             if (!ModelState.IsValid)
-            {        
-                return BadRequest(new Message() { Content = "Login data wrong"});
+            {
+                var errors = authService.GetErrors(ModelState);
+                return BadRequest(response.Failed("Login data wrong", errors));
             }
             
             var result = await signInManager.PasswordSignInAsync(request.UserName, request.Password, false, false);
             if (result != Microsoft.AspNetCore.Identity.SignInResult.Success)
             {
-                return Ok(new Message() { Content = "Login failed" });
+                return BadRequest(response.Failed("Login failed"));
             }
 
             var user = await userManager.FindByNameAsync(request.UserName);
-            var roles = await userManager.GetRolesAsync(user);
-            return authService.GetAuthData(user.Id, roles.FirstOrDefault());
+            var role = await roleManager.FindByIdAsync(user.RoleID.ToString());
+            return Ok(response.Succeed(authService.GetAuthData(user.Id, role.Name)));
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<ResponseResult<AuthResponse>>> Register([FromBody] RegisterRequest request)
+        {
+            ResponseResult<AuthResponse> response = new ResponseResult<AuthResponse>();
+            if (!ModelState.IsValid)
+            {
+                var errors = authService.GetErrors(ModelState);
+                return BadRequest(response.Failed("Register data wrong", errors));
+            }
+            
+            var user = await userManager.FindByNameAsync(request.UserName);
+            if (user != null)
+            {
+                return BadRequest(response.Failed("User name is already"));
+            }
+
+            user = await userManager.FindByNameAsync(request.Email);
+            if (user != null)
+            {
+                return BadRequest(response.Failed("Email is already"));
+            }
+
+            user = mapper.Map<RegisterRequest, AppUser>(request);
+            var result = await userManager.CreateAsync(user, request.Password);
+            if (result != IdentityResult.Success)
+            {
+                var errors = authService.GetErrors(result);
+                return BadRequest(response.Failed("Register failed",errors));
+            }
+
+            AppRole role = await roleManager.FindByNameAsync("client");
+            return Ok(response.Succeed(authService.GetAuthData(user.Id, role.Name)));
+        }
+
+        [HttpPost("changePassword")]
+        public async Task<ActionResult<ResponseResult<string>>> ChangePassword([FromBody] ChangePasswordRequest request)
+        {
+            ResponseResult<string> response = new ResponseResult<string>();
+            if (!ModelState.IsValid)
+            {
+                var errors = authService.GetErrors(ModelState);
+                return BadRequest(response.Failed("Change password data wrong", errors));
+            }
+            
+            var user = await userManager.FindByIdAsync(request.UserId.ToString());
+            if (user is null)
+            {
+                return BadRequest(response.Failed("Can not find user with Id: " + request.UserId.ToString()));
+            }
+
+            var result = await userManager.ChangePasswordAsync(user, request.Password, request.NewPassword);
+            if (result != IdentityResult.Success)
+            {
+                var errors = authService.GetErrors(result);
+                return BadRequest(response.Failed("Change password failed", errors));
+                
+            }
+
+            return Ok(response.Succeed("Succeed"));
+        }
+
+        [HttpPost("setRole")]
+        public async Task<ActionResult<ResponseResult<string>>> SetRole([FromBody] SetRoleRequest request)
+        {
+            ResponseResult<string> response = new ResponseResult<string>();
+            if (!ModelState.IsValid)
+            {
+                var errors = authService.GetErrors(ModelState);
+                return BadRequest(response.Failed("Change password data wrong", errors));
+            }
+
+            var user = await userManager.FindByIdAsync(request.UserId.ToString());
+            if (user is null)
+            {
+                return BadRequest(response.Failed("Can not find user with Id: " + request.UserId.ToString()));
+            }
+            var result = await userManager.AddToRolesAsync(user, request.Roles);
+            if (result != IdentityResult.Success)
+            {
+                var errors = authService.GetErrors(result);
+                return BadRequest(response.Failed("Set roles failed",errors));
+            }
+
+            return Ok(response.Succeed("Succeed"));
         }
     }
 }
