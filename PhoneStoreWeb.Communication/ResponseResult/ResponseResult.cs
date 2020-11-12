6c679758b3bc9ebc0d18349f478de3d1b4c87d59
nguyentan2212 <57379741+nguyentan2212@users.noreply.github.com>
2020-11-12 22:24:51 +0700

    Create response result for api

---
+++ PhoneStoreWeb.Communication/ResponseResult/ResponseResult.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PhoneStoreWeb.Communication.ResponseResult
+{
+    public class ResponseResult<TResult> where TResult : class
+    {
+        public bool IsSucceed { get; set; }
+        public string Message { get; set; }
+        public TResult Result { get; set; }
+        public IEnumerable<string> Errors { get; set; }
+
+        public ResponseResult<TResult> Succeed(TResult result)
+        {
+            IsSucceed = true;
+            Message = string.Empty;
+            Errors = Enumerable.Empty<string>();
+            Result = result;
+            return this;
+        }
+        public ResponseResult<TResult> Failed(string content)
+        {
+            IsSucceed = false;
+            Message = content;
+            Errors = Enumerable.Empty<string>();
+            Result = null;
+            return this;
+        }
+        public ResponseResult<TResult> Failed(string content, IEnumerable<string> errors)
+        {
+            IsSucceed = false;
+            Message = content;
+            Errors = (Errors ?? Enumerable.Empty<string>()).Concat(errors ?? Enumerable.Empty<string>());
+            Result = null;
+            return this;
+        }       
+    }
+}
