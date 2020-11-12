79bc4d3f3867822fd7217cc1b01d5fee9b5bea5a
nguyentan2212 <57379741+nguyentan2212@users.noreply.github.com>
2020-11-12 22:28:01 +0700

    Create mapper from request to model

---
+++ PhoneStoreWeb.Communication/Mapping/RequestToModel.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using PhoneStoreWeb.Communication.Authentication;
+using PhoneStoreWeb.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneStoreWeb.Communication.Mapping
+{
+    class RequestToModel : Profile
+    {
+        public RequestToModel()
+        {
+            CreateMap<RegisterRequest, AppUser>();
+        }
+    }
+}
