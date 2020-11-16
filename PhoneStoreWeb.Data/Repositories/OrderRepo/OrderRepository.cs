e96012a4b848bca0dc76f2682b40f5e469537802
nguyentan2212 <57379741+nguyentan2212@users.noreply.github.com>
2020-11-16 09:41:36 +0700

    Create order's repository

---
+++ PhoneStoreWeb.Data/Repositories/OrderRepo/OrderRepository.cs
@@ -0,0 +1,19 @@
+﻿using PhoneStoreWeb.Data.Repositories.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PhoneStoreWeb.Data.Models;
+using PhoneStoreWeb.Data.Contexts;
+
+namespace PhoneStoreWeb.Data.Repositories.OrderRepo
+{
+    public class OrderRepository : RepositoryBase<Order>, IOrderRepository
+    {
+        public OrderRepository(PhoneStoreDbContext dbContext) : base(dbContext)
+        {
+
+        }
+    }
+}
