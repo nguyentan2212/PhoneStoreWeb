e687383b9fb2c0e51a9fae01f628afff6062215a
nguyentan2212 <57379741+nguyentan2212@users.noreply.github.com>
2020-11-16 09:40:58 +0700

    Create product's repository

---
+++ PhoneStoreWeb.Data/Repositories/ProductRepo/IProductRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PhoneStoreWeb.Data.Repositories.Repository;
+using PhoneStoreWeb.Data.Models;
+
+namespace PhoneStoreWeb.Data.Repositories.ProductRepo
+{
+    public interface IProductRepository: IRepositoryBase<Product>
+    {
+
+    }
+}
---
+++ PhoneStoreWeb.Data/Repositories/ProductRepo/ProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using PhoneStoreWeb.Data.Contexts;
+using PhoneStoreWeb.Data.Models;
+using PhoneStoreWeb.Data.Repositories.Repository;
+
+namespace PhoneStoreWeb.Data.Repositories.ProductRepo
+{
+    public class ProductRepository : RepositoryBase<Product>, IProductRepository
+    {
+        public ProductRepository(PhoneStoreDbContext dbContext) : base(dbContext)
+        {
+
+        }
+    }
+}
