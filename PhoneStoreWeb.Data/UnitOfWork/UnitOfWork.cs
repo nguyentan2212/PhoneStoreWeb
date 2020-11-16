11ab425d3dd33f372ede4d04288715a868b67af2
nguyentan2212 <57379741+nguyentan2212@users.noreply.github.com>
2020-11-16 09:42:28 +0700

    Add repository properties

--- PhoneStoreWeb.Data/UnitOfWork/UnitOfWork.cs
+++ PhoneStoreWeb.Data/UnitOfWork/UnitOfWork.cs
@@ -1,20 +1,60 @@
 ﻿using PhoneStoreWeb.Data.Contexts;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using PhoneStoreWeb.Data.Repositories.ProductRepo;
+using PhoneStoreWeb.Data.Repositories.OrderRepo;
+using PhoneStoreWeb.Data.Repositories.BlogRepo;
 
 namespace PhoneStoreWeb.Data.UnitOfWork
 {
     public class UnitOfWork : IDisposable
     {
         private readonly PhoneStoreDbContext context;
+        private ProductRepository products;
+        private BlogRepository blogs;
+        private OrderRepository orders;
+
         public UnitOfWork()
         {
             context = new PhoneStoreDbContext();
         }
+        #region Repositories properties
+        public ProductRepository Products
+        {
+            get
+            { 
+                if (products is null)
+                {
+                    products = new ProductRepository(context);
+                }
+                return products; 
+            }           
+        }
         
+        public BlogRepository Blogs
+        {
+            get
+            {
+                if (blogs is null)
+                {
+                    blogs = new BlogRepository(context);
+                }
+                return blogs;
+            }
+        }
+
+        public OrderRepository Orders
+        {
+            get
+            {
+                if (orders is null)
+                {
+                    orders = new OrderRepository(context);
+                }
+                return orders;
+            }
+        }
+        #endregion
+
         public void Save()
         {
             context.SaveChanges();
