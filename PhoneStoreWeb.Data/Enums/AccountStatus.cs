ff6147fce9cccfd5025228dacf3a91835b95330d
nguyentan2212 <57379741+nguyentan2212@users.noreply.github.com>
2020-12-01 16:51:32 +0700

    add descriptions

--- PhoneStoreWeb.Data/Enums/AccountStatus.cs
+++ PhoneStoreWeb.Data/Enums/AccountStatus.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +9,9 @@ namespace PhoneStoreWeb.Data.Enums
 {
     public enum AccountStatus
     {
+        [Description("Kích hoạt")]
         Active,
+        [Description("Bị khóa")]
         Locked
     }
 }
