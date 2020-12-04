fed8741aebf319d7b8ca01547e7760895e87850c
nguyentan2212 <57379741+nguyentan2212@users.noreply.github.com>
2020-12-04 13:31:07 +0700

    add SoldDate property

--- PhoneStoreWeb.Communication/ResponseResult/ProductItemResponse.cs
+++ PhoneStoreWeb.Communication/ResponseResult/ProductItemResponse.cs
@@ -12,7 +12,8 @@ namespace PhoneStoreWeb.Communication.ResponseResult
         public int Id { get; set; }       
         public string SerialNumber { get; set; }      
         public decimal ReceivedPrice { get; set; }       
-        public DateTime ReceivedDate { get; set; }       
+        public DateTime ReceivedDate { get; set; }
+        public DateTime SoldDate { get; set; }
         public decimal SoldPrice { get; set; }
         public ProductItemStatus Status { get; set; }      
         public int WarrantyPeriod { get; set; }
