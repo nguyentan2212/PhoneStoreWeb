385cc9a96957db98c72d533e9e98620c88f89951
nguyentan2212 <57379741+nguyentan2212@users.noreply.github.com>
2020-12-06 18:22:59 +0700

    new ProductItemReceivedRequest class

---
+++ PhoneStoreWeb.Communication/Products/ProductItemReceivedRequest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneStoreWeb.Communication.Products
+{
+    public class ProductItemReceivedRequest
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string SerialNumber { get; set; }
+        [Required]
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "Money")]
+        public decimal ReceivedPrice { get; set; }
+    }
+}
