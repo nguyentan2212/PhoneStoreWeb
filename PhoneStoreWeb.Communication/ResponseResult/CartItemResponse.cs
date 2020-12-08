4a7b338da07104c47c31ee1ef5c073b867c95141
nguyentan2212 <57379741+nguyentan2212@users.noreply.github.com>
2020-12-08 15:12:34 +0700

    remove cartItem entity

--- PhoneStoreWeb.Communication/ResponseResult/CartItemResponse.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PhoneStoreWeb.Communication.ResponseResult
-{
-    public class CartItemResponse
-    {
-        public ProductResponse Product { get; set; }
-        public int CartID { set; get; }
-        public int Quantity { set; get; }
-    }
-}
