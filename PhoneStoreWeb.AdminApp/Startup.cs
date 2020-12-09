ba290a92ee8052f76bddc8688a0f7bbb6cfa7d9e
nguyentan2212 <57379741+nguyentan2212@users.noreply.github.com>
2020-12-09 11:03:47 +0700

    add order service to DI

--- PhoneStoreWeb.AdminApp/Startup.cs
+++ PhoneStoreWeb.AdminApp/Startup.cs
@@ -14,6 +14,7 @@ using PhoneStoreWeb.Data.Models;
 using PhoneStoreWeb.Service.CategoryService;
 using PhoneStoreWeb.Service.DiscountService;
 using PhoneStoreWeb.Service.FileService;
+using PhoneStoreWeb.Service.OrderService;
 using PhoneStoreWeb.Service.ProductService;
 using PhoneStoreWeb.Service.UserService;
 
@@ -60,7 +61,8 @@ namespace PhoneStoreWeb.AdminApp
             services.AddScoped<ICategoryService, CategoryService>();
             services.AddScoped<IProductService, ProductService>();
             services.AddScoped<IDiscountService, DiscountService>();
-            services.AddScoped<IUserService, UserService>();         
+            services.AddScoped<IUserService, UserService>();
+            services.AddScoped<IOrderService, OrderService>();
             services.AddMvc(options => options.Filters.Add(new AuthorizeFilter()));
         }
 
