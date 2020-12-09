ce0ffed077c2e6f375e9ace1308818ef2d1dc14c
nguyentan2212 <57379741+nguyentan2212@users.noreply.github.com>
2020-12-09 10:40:20 +0700

    Create order item

---
+++ PhoneStoreWeb.Communication/Orders/OrderItem.cs
@@ -0,0 +1,11 @@
+﻿namespace PhoneStoreWeb.Communication.Orders
+{
+    public class OrderItem
+    {
+        public int ProductItemId { get; set; }
+        public int WarrantyPeriod { get; set; }
+        public string Name { get; set; }
+        public string SerialNumber { get; set; }      
+        public decimal SoldPrice { get; set; }
+    }
+}
