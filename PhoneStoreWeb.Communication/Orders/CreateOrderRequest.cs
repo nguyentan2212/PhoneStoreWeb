b5fe9abeb2343998d125c7abe758b355ac46c54f
nguyentan2212 <57379741+nguyentan2212@users.noreply.github.com>
2020-12-09 10:40:07 +0700

    Create order request

---
+++ PhoneStoreWeb.Communication/Orders/CreateOrderRequest.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace PhoneStoreWeb.Communication.Orders
+{
+    public class CreateOrderRequest
+    {
+        public string AppUserId { get; set; }
+        public decimal TotalPrice { get; set; }
+        public decimal FinalPrice { get; set; }
+        public int DiscountId { get; set; }
+        public string ShipName { set; get; }
+        public string ShipEmail { set; get; }
+        public string ShipPhoneNumber { set; get; }
+        public string ShipAddress { set; get; }
+        public string OrderNotes { set; get; }
+        public List<OrderItem> Items { get; set; }
+    }
+}
