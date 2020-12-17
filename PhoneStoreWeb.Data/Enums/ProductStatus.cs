63992dfa22cb159ce3c592abfb0289b2d83aa3a9
nguyentan2212 <57379741+nguyentan2212@users.noreply.github.com>
2020-12-17 09:33:34 +0700

    add description

--- PhoneStoreWeb.Data/Enums/ProductStatus.cs
+++ PhoneStoreWeb.Data/Enums/ProductStatus.cs
@@ -5,11 +5,14 @@ namespace PhoneStoreWeb.Data.Enums
 {
     public enum ProductStatus
     {
-        [Display(Name = "Còn hàng")]        
+        [Display(Name = "Còn hàng")]   
+        [Description("Còn hàng")]
         InStock,
-        [Display(Name = "Hết hàng")]       
+        [Display(Name = "Hết hàng")] 
+        [Description("Hết hàng")]
         OutOfStock,
-        [Display(Name = "Ngừng bán")]       
+        [Display(Name = "Ngừng bán")]   
+        [Description("Ngừng bán")]
         SoldOut
     }
 }
