2093c1da8a4ab6bfc3ca77dfc4d4401bae41c490
nguyentan2212 <57379741+nguyentan2212@users.noreply.github.com>
2020-12-17 11:43:29 +0700

    create messageResponse class

---
+++ PhoneStoreWeb.Communication/ResponseResult/MessageResponse.cs
@@ -0,0 +1,15 @@
+﻿namespace PhoneStoreWeb.Communication.ResponseResult
+{
+    public class MessageResponse
+    {
+        public string Type { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public MessageResponse(string type, string title, string content = "")
+        {
+            Type = type;
+            Title = title;
+            Content = content;
+        }
+    }
+}
