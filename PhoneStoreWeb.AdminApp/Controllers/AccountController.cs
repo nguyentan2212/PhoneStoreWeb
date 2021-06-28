5eaa01e8ef47ed83a464bfa81de1538f85bc5c4e
nguyentan2212 <57379741+nguyentan2212@users.noreply.github.com>
2021-06-28 17:11:31 +0700

    update

--- PhoneStoreWeb.AdminApp/Controllers/AccountController.cs
+++ PhoneStoreWeb.AdminApp/Controllers/AccountController.cs
@@ -3,11 +3,8 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using PhoneStoreWeb.Communication.Authentication;
 using PhoneStoreWeb.Data.Models;
-using PhoneStoreWeb.Data.UnitOfWork;
 using PhoneStoreWeb.Service.UserService;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace PhoneStoreWeb.AdminApp.Controllers
@@ -36,9 +33,23 @@ namespace PhoneStoreWeb.AdminApp.Controllers
         [HttpPost]
         public async Task<IActionResult> Login([FromForm] LoginRequest request, [FromQuery] string returnUrl)
         {
+            string regex = "^[a-zA-Z0-9]+$";
+            if (!Regex.IsMatch(request.UserName,regex))
+            {
+                ViewData["title"] = "Đăng nhập thất bại!";
+                ViewData["message"] = "Username chỉ bao gồm chữ cái và chữ số.";
+                return View();
+            }
             AppUser user = await userManager.FindByNameAsync(request.UserName);
+            if (user is null)
+            {
+                ViewData["title"] = "Đăng nhập thất bại!";
+                ViewData["message"] = "Tài khoản của bạn không tồn tại.";
+                return View();
+            }
             PasswordHasher<AppUser> passwordHasher = new PasswordHasher<AppUser>();
             string passHash = passwordHasher.HashPassword(user, request.Password);
+
             if (user.PasswordHash == passHash && user.Status == Data.Enums.AccountStatus.Locked)
             {
                 ViewData["title"] = "Đăng nhập thất bại!";
